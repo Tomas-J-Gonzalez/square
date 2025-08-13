c3adcff623149eb8e3966fc3d78ecec27855a3fa
Tomás González <tomachos08@gmail.com>
2025-08-14 10:25:43 +1200

    edit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'assets',
+  // Put static files into the root of the build, e.g. /logo.png
+  publicDir: '.',
   resolve: {
     alias: {
       '@': '/src',
